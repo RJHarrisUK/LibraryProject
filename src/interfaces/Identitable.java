6a2280e73655d2f788ab1ab7c7374ec355c027b6
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-05-23 15:27:14 +0100

    first upload

---
+++ src/interfaces/Identitable.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Identitable {
+
+}
---
+++ src/models/Item.java
@@ -0,0 +1,15 @@
+package models;
+
+public abstract class Item {
+
+	// attributes
+	private int id;
+	private String name;
+	private String publisher;
+
+	// constructor
+
+	// behaviours
+
+	// getters and setters
+}
